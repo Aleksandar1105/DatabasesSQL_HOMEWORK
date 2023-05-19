21393a8b5abca874bbec003554f522b7e9c696b6
Aleksandar1105 <todoroski.aleksandar@gmail.com>
2023-05-19 22:31:21 +0200

    Added Homework#2

--- Homework#1/SQLQuery4.sql
+++ Homework#1/SQLQuery4.sql
@@ -1,24 +1,15 @@
-USE [master]
+USE [SEDCHome]
 GO
 
-DROP DATABASE IF EXISTS [SEDCACADEMYDB]
-GO
-
-CREATE DATABASE [SEDCACADEMYDB]
-GO
-
-USE [SEDCACADEMYDB]
-GO
-
-DROP TABLE IF EXISTS [dbo].[Students]
-DROP TABLE IF EXISTS [dbo].[Teachers]
-DROP TABLE IF EXISTS [dbo].[Grades]
-DROP TABLE IF EXISTS [dbo].[Courses]
-DROP TABLE IF EXISTS [dbo].[AchievementTypes]
+DROP TABLE IF EXISTS [dbo].[Student]
+DROP TABLE IF EXISTS [dbo].[Teacher]
+DROP TABLE IF EXISTS [dbo].[Grade]
+DROP TABLE IF EXISTS [dbo].[Course]
+DROP TABLE IF EXISTS [dbo].[AchievementType]
 DROP TABLE IF EXISTS [dbo].[GradeDetails]
 GO
 
-CREATE TABLE Students (
+CREATE TABLE Student (
 Id int IDENTITY (1,1) NOT NULL,
 FirstName nvarchar(100) NOT NULL,
 LastName nvarchar(100) NOT NULL,
@@ -26,52 +17,52 @@ DateOfBirth date NULL,
 EnrolledDate date NOT NULL,
 Gender nchar(1) NOT NULL,
 NationalIDNumber nvarchar(50) NULL,
-StudentCardNumber bigint NOT NULL,
-CONSTRAINT [PK_Students] PRIMARY KEY (Id)
+StudentCardNumber nvarchar(50) NOT NULL,
+CONSTRAINT [PK_Student] PRIMARY KEY (Id)
 )
 GO
 
-CREATE TABLE Teachers (
+CREATE TABLE Teacher (
 Id int IDENTITY (1,1) NOT NULL,
 FirstName nvarchar(100) NOT NULL,
 LastName nvarchar(100) NOT NULL,
 DateOfBirth date NULL,
-AcademicRank int NULL,
+AcademicRank nvarchar(50) NOT NULL,
 HireDate date NOT NULL,
-CONSTRAINT [PK_Teachers] PRIMARY KEY (Id)
+CONSTRAINT [PK_Teacher] PRIMARY KEY (Id)
 )
 GO
 
-CREATE TABLE Grades (
+CREATE TABLE Grade (
 Id int IDENTITY (1,1) NOT NULL,
 StudentID int NOT NULL,
 CourseID int NOT NULL,
-TeacherID int NOT NULL,
-Grade int NOT NULL,
+TeacherID int NULL,
+Grade int NULL,
 Comment nvarchar(100) NOT NULL,
 CreatedDate date NOT NULL,
-CONSTRAINT [PK_Grades] PRIMARY KEY (Id)
+CONSTRAINT [PK_Grade] PRIMARY KEY (Id)
 )
 GO
 
-CREATE TABLE Courses (
+CREATE TABLE Course (
 Id int IDENTITY (1,1) NOT NULL,
 [Name] nvarchar(100) NOT NULL,
 Credit nvarchar(50) NOT NULL,
-AcademicYear date NOT NULL,
+AcademicYear int NOT NULL,
 Semester int NULL,
-Comment nvarchar(100) NOT NULL,
-CreatedDate date NOT NULL,
-CONSTRAINT [PK_Courses] PRIMARY KEY (Id)
+Comment nvarchar(100) NULL,
+CreatedDate int NULL,
+CONSTRAINT [PK_Course] PRIMARY KEY (Id)
 )
 GO
 
-CREATE TABLE AchievementTypes (
+CREATE TABLE AchievementType (
 Id int IDENTITY (1,1) NOT NULL,
 [Name] nvarchar(100) NOT NULL,
 [Description] nvarchar(max) NULL,
 ParticipationRate int NOT NULL,
-CONSTRAINT [PK_AchievementTypes] PRIMARY KEY (Id)
+CONSTRAINT [PK_AchievementType] PRIMARY KEY (Id)
 )
 GO
 
@@ -86,27 +77,3 @@ CONSTRAINT [PK_GradeDetails] PRIMARY KEY (Id)
 )
 GO
 
---INSERT INTO Students (FirstName, LastName, DateOfBirth, EnrolledDate, Gender, NationalIDNumber, StudentCardNumber, City)
---VALUES 
---('Aleksandar', 'Todoroski', '1985-05-11', '2022-10-17', 'M', 12345678944, 25221,'Skopje'),
---('Dragica', 'Todoroska', '1988-01-20', '2022-10-17', 'M', 12151684684, 21566,'Skopje');
---GO
-
-ALTER TABLE Students
-ADD City nvarchar(100);
-GO
-
-INSERT INTO Students (FirstName, LastName, DateOfBirth, EnrolledDate, Gender, NationalIDNumber, StudentCardNumber, City)
-VALUES 
-('Aleksandar', 'Todoroski', '1985-05-11', '2022-10-17', 'M', 12345678944, 25221,'Skopje'),
-('Dragica', 'Todoroska', '1988-01-20', '2022-10-17', 'F', 12151684684, 21566,'Skopje');
-GO
-
-SELECT * FROM Students
-
-UPDATE Students
-SET City = 'Struga',
-    StudentCardNumber = 12214
-WHERE Id = 2;
-
-SELECT * FROM Students
---
+++ Homework#2/Homework#2.sql
@@ -0,0 +1,45 @@
+--Find all Students with FirstName = Antonio
+SELECT *
+FROM Student
+WHERE FirstName = 'Antonio'
+GO
+
+--Find all Students with DateOfBirth greater than '01.01.1999'
+SELECT *
+FROM Student
+WHERE DateOfBirth > '01.01.1999'
+GO
+
+--Find all Students with LastName starting With ‘J’ enrolled in January/1998
+SELECT *
+FROM Student
+WHERE LastName LIKE 'J%' AND EnrolledDate > '1998-01-01' AND EnrolledDate < '1998-01-31'
+GO
+
+--List all Students ordered by FirstName
+SELECT *
+FROM Student
+ORDER BY FirstName
+GO
+
+--List all Teacher Last Names and Student Last Names in single result set. Remove duplicates
+SELECT LastName FROM Teacher
+UNION
+SELECT LastName FROM Student
+GO
+
+--Create Foreign key constraints from diagram or with script
+--Created with diagram !
+
+
+--List all possible combinations of Courses names and AchievementType names that can be passed by student
+SELECT c.[Name] AS CourseName, a.[Name] AS AchievementTypeName
+FROM Course c
+CROSS JOIN AchievementType a
+GO
+
+--List all Teachers without exam Grade
+SELECT t.*
+FROM Teacher t
+LEFT JOIN Grade g ON t.Id = g.TeacherID
+WHERE g.Id IS NULL
